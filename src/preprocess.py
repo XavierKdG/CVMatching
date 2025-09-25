8c89fa7bb0956cff832c2b7db2af601c288e40fe
XavierKdG <xavier.korffdegidts@gmail.com>
2025-09-25 16:14:26 +0000

    extra parameter toegevoegd voor lemmatization/stemming + comment met waar ik evt. nog aan wil werken voor preprocessing

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,21 +5,29 @@ import spacy
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 from sentence_transformers import SentenceTransformer
 
 os.makedirs('data/processed', exist_ok=True)
 os.makedirs('data/raw', exist_ok=True)
 
-# from sentence_transformers import SentenceTransformer
-# model = SentenceTransformer("all-MiniLM-L6-v2")
-# print(model.encode(["hello world"]))
+# Nog toe te voegen misschien:
+# elongation, negation handling, spellchecker
 
 class TextPreprocessing:
-    def __init__(self, model_name='all-MiniLM-L6-v2'):
-        nltk.download('stopwords') #download once
+    def __init__(self, model_name='all-MiniLM-L6-v2', lemmatization=True):
+        nltk.download('stopwords', quiet=True) #download once
+        nltk.download('wordnet', quiet=True) #download once
+
         self.nlp = spacy.load("en_core_web_sm") #spacy model
         self.stop_words = set(stopwords.words('english')) #for now english stopwords only
-        self.stemmer = SnowballStemmer("english") #stemming because wai hong uses lemmatization
+
+        self.use_lemmatizer = lemmatization
+        if self.use_lemmatizer:
+            self.processor = WordNetLemmatizer() #uses lemmatizer
+        else:
+            self.processor = SnowballStemmer("english") #uses stemmer
 
         self.bert_model = SentenceTransformer(model_name) #BERT model 
 
@@ -41,8 +49,12 @@ class TextPreprocessing:
     def tokenize_and_stem(self, text):
         doc = self.nlp(text)
         tokens = [token.text for token in doc if token.text.lower() not in self.stop_words]
-        stemmed = [self.stemmer.stem(token) for token in tokens]
-        return stemmed
+        
+        if self.use_lemmatizer:
+            processed = [self.processor.lemmatize(token) for token in tokens]
+        else:
+            processed = [self.processor.stem(token) for token in tokens]
+        return processed
     
     def preprocess_texts(self, texts):
         return [self.tokenize_and_stem(self.clean_text(t)) for t in texts]
@@ -55,15 +67,15 @@ def load_raw_csv(file_path):
     return pd.read_csv(file_path)
 
 def main():
-    df1 = pd.read_csv("./data/raw/job_descriptions.csv")
+    # df1 = pd.read_csv("./data/raw/job_descriptions.csv")
     df2 = pd.read_csv("./data/raw/job_descriptions2.csv")
 
     preprocessor = TextPreprocessing()
 
-    # preproccessing df1
-    df1_columns_to_process = ['Job Description', 'Skills', 'Experience', 'Responsibilities']
-    df1_processed = preprocessor.preprocess_dataframe(df1, df1_columns_to_process)
-    df1_processed.to_csv('./data/processed/processed_df1.csv', index=False)
+    # # preproccessing df1
+    # df1_columns_to_process = ['Job Description', 'Skills', 'Experience', 'Responsibilities']
+    # df1_processed = preprocessor.preprocess_dataframe(df1, df1_columns_to_process)
+    # df1_processed.to_csv('./data/processed/processed_df1.csv', index=False)
 
     # preproccessing df2
     df2_columns_to_process = ['Job Description', 'Preferred Skills']
