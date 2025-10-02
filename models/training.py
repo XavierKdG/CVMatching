eec881f65e3df636981f47f6a58b37591bfed481
quintycf <170643485+quintycf@users.noreply.github.com>
2025-10-02 11:23:10 +0000

    UI

--- models/training.py
+++ models/training.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
+import pandas as pd
 import joblib
 
 X = pd.read_csv("data/processed/tfidf_matrix.csv")
@@ -16,6 +17,5 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print(classification_report(y_test, y_pred))
-
 joblib.dump(model, "models/tfidf.pkl")
 print("Model opgeslagen")
