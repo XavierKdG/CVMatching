df15513c195fd31d468decc018fe1a048f2b0364
XavierKdG <xavier.korffdegidts@gmail.com>
2025-10-14 23:32:51 +0000

    added utils.py for help functions (setup_logging and load_config) in scripts

---
+++ src/utils.py
@@ -0,0 +1,34 @@
+import logging
+import os
+import yaml
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+def setup_logging(log_file_name='pipeline.log'):
+    """Configure logging to log_file_name"""
+    os.makedirs('logs', exist_ok=True)
+    log_path = os.path.join('logs', log_file_name)
+
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+
+    def amsterdam_time(*args):
+        return datetime.now(ZoneInfo("Europe/Amsterdam")).timetuple() # timezone amsterdam CEST instead of default UTC
+
+    logging.Formatter.converter = amsterdam_time
+        
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(log_path, mode='a'),
+            logging.StreamHandler() 
+        ]
+    )
+    logging.info(f"Logging configured. Output added: {log_path}")
+
+def load_config(config_path="configs/config.yml"):
+    """Load configuration from a YAML file."""
+    logging.info(f"Configuration loaded from: {config_path}")
+    with open(config_path, "r") as f:
+        return yaml.safe_load(f)
