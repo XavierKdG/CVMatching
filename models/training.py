bcfeb8aa938efcbd251aa74423900c3cc41e4f5a
quintycf <170643485+quintycf@users.noreply.github.com>
2025-10-15 09:28:12 +0000

    training.py

--- models/training.py
+++ models/training.py
@@ -3,19 +3,32 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
 import pandas as pd
 import joblib
+import os
 
-X = pd.read_csv("..data/processed/tfidf_matrix.csv")
-y = pd.read_csv("..data/processed/labeled_datatest.csv")["label"]
+#data inladen
+X = pd.read_csv("../data/processed/tfidf_matrix.csv")
+y = pd.read_csv("../data/processed/labeled_datatest.csv")["label"]
 
+print("X shape:", X.shape)
+print("y shape:", y.shape)
+
+#train/test split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+#model trainen
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
+#voorspellingen
 y_pred = model.predict(X_test)
+
+#evaluatie
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print(classification_report(y_test, y_pred))
+
+#model opslaan
+os.makedirs("models", exist_ok=True)
 joblib.dump(model, "models/tfidf.pkl")
-print("Model opgeslagen")
+print("Model opgeslagen in models/tfidf.pkl")
