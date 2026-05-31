da6c9793b0c755c4f53842e09ffc553203863f70
Xavier <xavier.korffdegidts@gmail.com>
2026-05-31 10:28:45 +0200

    Imports niet meer nodig

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .preprocess import main as preprocess_data
-from .train import main as train_model
-from .upload_to_qdrant import main as upload_to_qdrant
-from .utils import setup_logging, load_config
